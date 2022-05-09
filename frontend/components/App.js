d301ca799b1d672fb7a3a5340bc7a954d1f7c47b
Kevin Lai <kevin.lai@gmail.com>
2022-05-09 14:17:43 -0400

    initial commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1> Todos </h1>
+          <ul>
+            <li>Do the laundry</li>
+            <li>Wash the dishes</li>
+            <li>Clean room</li>
+          </ul>
+
+          <form>
+            <input/>
+            <button> Add </button>
+          </form>
+
+          <button> Clear </button>
       </div>
     )
   }
