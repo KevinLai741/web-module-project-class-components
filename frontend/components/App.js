871e85e83e46e38df94b7762738d4875e487dd93
Kevin Lai <kevin.lai@gmail.com>
2022-05-09 14:26:06 -0400

    added todos in component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: true
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <h1> Todos </h1>
           <ul>
-            <li>Do the laundry</li>
-            <li>Wash the dishes</li>
-            <li>Clean room</li>
+            { 
+              todos.map(todo => {
+                return (<li>{todo.task} { todo.completed ? <span> - completed - </span> : <span> - incomplete - </span> }</li>)
+              })
+            }
           </ul>
 
           <form>
